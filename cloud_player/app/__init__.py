555ab36b0086dd545c12e42bb93aaa81e19f90f4
952079739@qq.com <952079739@qq.com>
2019-11-23 18:50:35 +0800

    four

--- cloud_player/app/__init__.py
+++ cloud_player/app/__init__.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost:3306/flask?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.secret_key = '123456'
+app.debug = True
 db = SQLAlchemy(app)
 
 from app import routes
